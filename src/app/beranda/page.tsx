d1a637e0ea578992a6c2af2518cd11a6c70ea325
mhmdyoga <yogaskie@gmail.com>
2025-09-14 22:53:37 +0700

    modify: samething

--- src/app/beranda/page.tsx
+++ src/app/beranda/page.tsx
@@ -162,7 +162,7 @@ const Beranda = () => {
         </div>
         <h2 className='text-[#111] font-bold text-3xl p-4'>New Arrival</h2>
         <div className=''>
-           <ScrollArea className='w-full h-[400px] px-4 flex gap-4'>
+           <ScrollArea className='w-[400px] h-[400px] px-4 flex gap-4'>
            {data?.map((item: any, index: number) => (
               <div key={item.id}>
                 <div className="flex flex-col gap-2">
